244d255be8ff0cdd9595ff7b0e1a8a094cbe9ac8
Pritesh Jha <priteshjha4u@gmail.com>
2019-02-28 19:27:14 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import { SlateInitialValueSimple } from './components/mock.data';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    value: Value.fromJSON(SlateInitialValueSimple)
+  };
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <React.Fragment>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+            React Slate POC
           </a>
-        </header>
-      </div>
+        </nav>
+
+        <div className="container-fluid pb-5">
+          <div className="row">
+            <div className="col-md-12">
+              <Editor value={this.state.value} onChange={this.onChange} />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
